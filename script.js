2c1f248e0602e4bbbf6f6f6c10ffdd81d5a1f990
Meagan <meamckee89@gmail.com>
2022-07-07 09:37:17 -0400

    updated code. codealong

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 const body = document.querySelector("body");
-const cellContainer = document.querySelector(".cell-container");
+const list = document.querySelector("ul"); // make parent a variable to put Event Delegation on this
 const cells = document.querySelectorAll(".cell"); // returns an array of all the "cells"
-const randomInt = Math.floor(Math.random() * 16);
+const randomInt = Math.floor(Math.random() * 16); // 16 cells, but it starts at 0, so times by 16 not 17
+cells[randomInt].id = "it"; //makes the id "it"
 
-const chosenOne = cells[randomInt].setAttribute("id", "it"); //setting the attribute id of IT
-
-const makeActive = (event) => {
-  if (event.target.classList.contains("cell")) {
-    // first we want to see if that square has a class of "cell"
-    event.target.classList.add("active");
+const clickFunction = (e) => {
+  if (e.target.id === "it") {
+    e.target.style.backgroundColor = "red";
+    e.target.textContent = "IT";
+    body.style.backgroundColor = "black";
+    list.removeEventListener("click", clickFunction);
+  } else if (e.target.classList.contains("cell")) {
+    e.target.style.backgroundColor = "green";
   }
 };
 
-cellContainer.addEventListener("click", makeActive);
-
-cellContainer.addEventListener("click", (event) => {
-  if (event.target.getAttribute("id") === "it") {
-    //this is listening for id = to it to run different function
-    body.classList.add("gameover"); // adds the class of game over
-    event.target.classList.add("red"); // adds the class or red
-    event.target.textContent = "IT"; // adds the text content when the box is clicked
-    cellContainer.removeEventListener("click", makeActive); // removes the eventListener
-  }
-});
+list.addEventListener("click", clickFunction);
